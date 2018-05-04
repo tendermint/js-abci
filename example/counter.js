89f328449404a50c10053b2d9914f07a0558b77e
Matt Bell <mappum@gmail.com>
2018-05-04 10:50:32 -0700

    Update example

--- example/counter.js
+++ example/counter.js
@@ -8,33 +8,33 @@ let state = {
 }
 
 let handlers = {
-  info (request, cb) {
-    cb(null, {
+  info (request) {
+    return {
       data: 'Node.js counter app',
       version: '0.0.0',
       lastBlockHeight: 0,
       lastBlockAppHash: Buffer.alloc(0)
-    })
-  },
-
-  checkTx (request, cb) {
-    let number = request.tx.readUInt32BE(0)
-    if (number !== state.count) {
-      return cb(null, { code: 1, log: 'tx does not match count' })
     }
-    cb(null, { code: 0, log: 'tx succeeded' })
   },
 
-  deliverTx (request, cb) {
+  checkTx (request) {
     let number = request.tx.readUInt32BE(0)
     if (number !== state.count) {
-      return cb(null, { code: 1, log: 'tx does not match count' })
+      return { code: 1, log: 'tx does not match count' }
+    }
+    return { code: 0, log: 'tx succeeded' }
+  },
+
+  deliverTx (request) {
+    let number = request.tx.readUInt32BE(0)
+    if (number !== state.count) {
+      return { code: 1, log: 'tx does not match count' }
     }
 
     // update state
     state.count += 1
 
-    cb(null, { code: 0, log: 'tx succeeded' })
+    return { code: 0, log: 'tx succeeded' }
   }
 }
 
